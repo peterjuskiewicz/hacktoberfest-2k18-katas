e0afacdccaaec585d54d29a7348b72952132ffaa
Peter Yuskevich <peter@juskiewi.cz>
2018-10-03 07:35:01 +0100

    implement merge function

--- src/merge.js
+++ src/merge.js
@@ -1,3 +1,22 @@
-export const merge = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const merge = (...args) => {
+
+  if(args.length === 0){
+    return null;
+  }
+  if(args.length === 1){
+    return args[0];
+  }
+  if(args.length === 2){
+    let object1 = args[0];
+    let extended = args[1];
+    for(let prop in args[0]){
+        if(object1.hasOwnProperty(prop) && extended.hasOwnProperty(prop)){
+            extended[prop] = [object1[prop], extended[prop]];
+        }
+        else if(object1.hasOwnProperty(prop)){
+            extended[prop] = object1[prop];
+        }
+    }
+    return extended;
+  }
 };
